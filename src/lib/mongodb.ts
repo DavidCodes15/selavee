af2c6a5a306e9ff909c350bf6b523437c47aad23
david magaldadze <davidcodes@davids-MacBook-Air.local>
2024-08-31 20:05:12 +0400

    mongodb error fixing

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-// import { boolean } from "zod";
 
-let isConnected = false; // track the connection
+
+let isConnected = false; 
 export const connectToDB = async () => {
   if (isConnected) return mongoose.connection;
   console.log(process.env.MONGODB_URL);
@@ -11,12 +11,9 @@ export const connectToDB = async () => {
   try{
     await mongoose.connect(`${process.env.MONGODB_URL}`, {
         dbName: "selavee-database",
-        // useNewUrlParser: true,
-        // useUnifiedTopology: true,
         connectTimeoutMS: 10000,  // Timeout after 10 seconds
         socketTimeoutMS: 45000,   // Close sockets after 45 seconds of inactivity
         serverSelectionTimeoutMS: 5000,
-    //   useUnifiedTopology: true,
 
     })
     isConnected = true;
