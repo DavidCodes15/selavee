131e8cad9fa3a239ed2a4700694a6287dc1d64cc
david magaldadze <davidcodes@davids-MacBook-Air.local>
2024-08-31 19:56:09 +0400

    added some stuff to mongo connection

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -6,12 +6,16 @@ export const connectToDB = async () => {
   if (isConnected) return mongoose.connection;
   console.log(process.env.MONGODB_URL);
   
-  mongoose.set('strictQuery', true);
+  
 
   try{
     await mongoose.connect(`${process.env.MONGODB_URL}`, {
         dbName: "selavee-database",
-       
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+        connectTimeoutMS: 10000,  // Timeout after 10 seconds
+        socketTimeoutMS: 45000,   // Close sockets after 45 seconds of inactivity
+        serverSelectionTimeoutMS: 5000,
     //   useUnifiedTopology: true,
 
     })
